06fe66939821aee4c32620ec415bc26d0ef21be6
cyy <cyyever@outlook.com>
2024-03-02 09:41:34 +0800

    Fix code

--- cyy_torch_vision/model/__init__.py
+++ cyy_torch_vision/model/__init__.py
@@ -1,18 +1,19 @@
 import functools
 
-import torch
-from cyy_naive_lib.log import get_logger
 from cyy_torch_toolbox import DatasetType
 from cyy_torch_toolbox.dataset.collection import DatasetCollection
 from cyy_torch_toolbox.factory import Factory
 from cyy_torch_toolbox.model import (create_model,
                                      global_model_evaluator_factory,
                                      global_model_factory)
+from cyy_torch_toolbox.model.repositary import (get_model_info,
+                                                get_torch_hub_model_info)
 
+from ..dataset.util import VisionDatasetUtil
 from .evaluator import VisionModelEvaluator
 
 
-def get_model_evaluator(model, **kwargs):
+def get_model_evaluator(model, **kwargs) -> VisionModelEvaluator:
     return VisionModelEvaluator(model=model, **kwargs)
 
 
@@ -24,6 +25,7 @@ def get_model(
 ) -> dict:
     final_model_kwargs: dict = kwargs
     dataset_util = dataset_collection.get_dataset_util()
+    assert isinstance(dataset_util, VisionDatasetUtil)
     for k in ("input_channels", "channels"):
         if k not in kwargs:
             final_model_kwargs |= {
@@ -36,33 +38,13 @@ def get_model(
 def get_model_constructors() -> dict:
     model_info: dict = {}
     github_repos: list = [
-        "pytorch/vision:main",
         "huggingface/pytorch-image-models:main",
-        "cyyever/torch_models:main",
+        "pytorch/vision:main",
     ]
 
     for repo in github_repos:
-        entrypoints = torch.hub.list(
-            repo, force_reload=False, trust_repo=True, skip_validation=True
-        )
-        for model_name in entrypoints:
-            if model_name.lower() not in model_info:
-                model_info[model_name.lower()] = {
-                    "name": model_name,
-                    "constructor": functools.partial(
-                        torch.hub.load,
-                        repo_or_dir=repo,
-                        model=model_name,
-                        force_reload=False,
-                        trust_repo=True,
-                        skip_validation=True,
-                        verbose=False,
-                    ),
-                    "repo": repo,
-                }
-            else:
-                get_logger().debug("ignore model_name %s", model_name)
-
+        model_info |= get_torch_hub_model_info(repo)
+    model_info |= get_model_info()[DatasetType.Vision]
     return model_info
 
 
