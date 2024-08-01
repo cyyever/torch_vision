4c075e351fd406ba7ca61776c69fc64b2c95f01e
cyy <cyyever@outlook.com>
2024-08-01 09:26:10 +0800

    Fix test

--- cyy_torch_vision/test/test_dataset.py
+++ cyy_torch_vision/test/test_dataset.py
@@ -1,15 +1,12 @@
-import cyy_torch_vision
 from cyy_torch_toolbox import MachineLearningPhase, create_dataset_collection
-from cyy_torch_toolbox.data_pipeline.dataset import get_dataset_size
 
 
 def test_dataset() -> None:
     mnist = create_dataset_collection("MNIST")
-    mnist_training = mnist.get_dataset(MachineLearningPhase.Training)
     assert (
         abs(
-            get_dataset_size(mnist_training)
-            / get_dataset_size(mnist.get_dataset(MachineLearningPhase.Validation))
+            len(mnist.get_dataset_util(MachineLearningPhase.Training))
+            / len(mnist.get_dataset_util(MachineLearningPhase.Validation))
             - 12
         )
         < 0.01
@@ -20,8 +17,8 @@ def test_dataset() -> None:
     assert cifar10.get_dataset_util().channel == 3
     assert len(cifar10.get_dataset_util().get_labels()) == 10
     assert abs(
-        get_dataset_size(cifar10.get_dataset(MachineLearningPhase.Test))
-        - get_dataset_size(cifar10.get_dataset(MachineLearningPhase.Validation))
+        len(cifar10.get_dataset_util(MachineLearningPhase.Test))
+        - len(cifar10.get_dataset_util(MachineLearningPhase.Validation))
         <= 1
     )
     print("cifar10 labels are", cifar10.get_label_names())
