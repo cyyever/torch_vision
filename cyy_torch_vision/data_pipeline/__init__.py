f66bbc28bff2090bed5551786e3fd6318ddc86bc
cyy <cyyever@outlook.com>
2024-12-06 17:12:31 +0800

    Add code

--- cyy_torch_vision/data_pipeline/__init__.py
+++ cyy_torch_vision/data_pipeline/__init__.py
@@ -1,7 +1,7 @@
 from cyy_torch_toolbox import DatasetType
 from cyy_torch_toolbox.data_pipeline import global_data_transform_factory
 
-from .transform import add_vision_extraction, add_vision_transforms
+from .transform import add_vision_transforms
 
 
 def append_transforms_to_dc(dc, model_evaluator=None) -> None:
